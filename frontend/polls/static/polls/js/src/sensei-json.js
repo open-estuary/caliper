bae257a707f5468bba5c3795146dface854bc8db
Elaine Wu <wuyanjun1027@163.com>
2015-08-20 14:52:50 +0800

    fix little bugs in the frontend

--- frontend/polls/static/polls/js/src/sensei-json.js
+++ frontend/polls/static/polls/js/src/sensei-json.js
@@ -96,40 +96,40 @@ function getHoriData(json, columns){
 
 function draw_grid(grid){
     // register editors that are bundled with sensei grid
-    grid.registerEditor(BasicEditor);
-    grid.registerEditor(BooleanEditor);
-    grid.registerEditor(TextareaEditor);
-    grid.registerEditor(SelectEditor);
-    grid.registerEditor(DateEditor);
+    //grid.registerEditor(BasicEditor);
+    //grid.registerEditor(BooleanEditor);
+    //grid.registerEditor(TextareaEditor);
+    //grid.registerEditor(SelectEditor);
+    //grid.registerEditor(DateEditor);
     //grid.registerEditor(DisabledEditor);
 
     // example listeners on grid events
-    grid.events.on("editor:save", function (data, $cell) {
-        console.info("save cell:", data, $cell);
-    });
-    grid.events.on("editor:load", function (data, $cell) {
-        console.info("set value in editor:", data, $cell);
-    });
-    grid.events.on("cell:select", function ($cell) {
-        console.info("active cell:", $cell);
-    });
-    grid.events.on("cell:clear", function (oldValue, $cell) {
-        console.info("clear cell:", oldValue, $cell);
-    });
-    grid.events.on("cell:deactivate", function ($cell) {
-        console.info("cell deactivate:", $cell);
-    });
-    grid.events.on("row:select", function ($row) {
-        console.info("row select:", $row);
-    });
-    grid.events.on("row:save", function (data, $row, source) {
-        console.info("row save:", source, data);
-        // save row via ajax or any other way
-        // simulate delay caused by ajax and set row as saved
-        setTimeout(function () {
-            grid.setRowSaved($row);
-        }, 1000);
-    });
+    //grid.events.on("editor:save", function (data, $cell) {
+    //    console.info("save cell:", data, $cell);
+    //});
+    //grid.events.on("editor:load", function (data, $cell) {
+    //    console.info("set value in editor:", data, $cell);
+    //});
+    //grid.events.on("cell:select", function ($cell) {
+    //    console.info("active cell:", $cell);
+    //});
+    //grid.events.on("cell:clear", function (oldValue, $cell) {
+    //    console.info("clear cell:", oldValue, $cell);
+    //});
+    //grid.events.on("cell:deactivate", function ($cell) {
+    //    console.info("cell deactivate:", $cell);
+    //});
+    //grid.events.on("row:select", function ($row) {
+    //    console.info("row select:", $row);
+    //});
+    //grid.events.on("row:save", function (data, $row, source) {
+    //    console.info("row save:", source, data);
+    //    // save row via ajax or any other way
+    //    // simulate delay caused by ajax and set row as saved
+    //    setTimeout(function () {
+    //        grid.setRowSaved($row);
+    //    }, 1000);
+    //});
 
     // render grid
 	grid.render();
