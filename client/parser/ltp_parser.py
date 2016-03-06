d906da6ff3fcb432cbbfdba0fe80d2015a3ed362
Shyju PV <shyju.pv@huawei.com>
2016-03-06 17:14:52 +0530

    Latest LTP update, LTP test case organisation, tiny membench tool for memory testing

--- client/parser/ltp_parser.py
+++ client/parser/ltp_parser.py
@@ -5,35 +5,36 @@ import sys
 def ltpResult(content, outfp):
     sumPass = 0
     sumFail = 0
-    sumInfo = 0
+    sumConf = 0
     sumBrok = 0
     score = 0.0
-
-    if len(re.findall("TFAIL", content)):
-        sumFail = sumFail + len(re.findall("TFAIL", content))
-    if len(re.findall("TBROK", content)):
-        sumBrok = sumBrok + len(re.findall("TBROK", content))
-    if len(re.findall("TPASS", content)):
-        sumPass = sumPass + len(re.findall("TPASS", content))
-    if len(re.findall("TINFO", content)):
-        sumInfo = sumInfo + len(re.findall("TINFO", content))
-
-    outfp.write('the total number of testcases is %d\n'
-                    % (sumPass + sumFail + sumInfo + sumBrok))
-    outfp.write('the tesecases passed are %d, failed are %d\n'
-                    % (sumPass, sumFail))
+    sumTotal = 0
+    if len(re.findall("FAIL", content)):
+       sumFail = sumFail + len(re.findall("FAIL", content))
+    if len(re.findall("BROK", content)):
+       sumBrok = sumBrok + len(re.findall("BROK", content))
+    if len(re.findall("PASS", content)):
+       sumPass = sumPass + len(re.findall("PASS", content))
+    if len(re.findall("CONF", content)):
+        sumConf = sumConf + len(re.findall("CONF", content))
+ 
+    sumTotal = sumPass + sumFail + sumConf + sumBrok
+    outfp.write('\nthe total number of testcases:  %d\n'
+		    % (sumTotal))
+    outfp.write('the number of testcases passed are: %d\nthe number of failed testcases are: %d\n'
+                  % (sumPass, sumFail))
+    outfp.write('the number of testcases skipped are: %d\n \n'
+		     % (sumBrok + sumConf))
     try:
-        score = (0.0 + sumPass) / (sumPass + sumFail + sumInfo + sumBrok)
+        score = (0.0 + sumPass) / (sumTotal)
     except Exception:
         score = 0.0
     return score
-
 
 def ltp_parser(content, outfp):
     score = -1
     score = ltpResult(content, outfp)
     return score
-
 
 if __name__ == "__main__":
     fp = open(sys.argv[1], "r")
