a5ea0d30219d08e08b80944753ace8a01689474b
dikshit n <dikshit1805@gmail.com>
2016-05-30 14:05:07 +0530

    Revert "BUG FIX: Handling new line error while reading the testlist file for commenting/uncommenting the testcases by the automation script"
    This reverts commit fde375c185bc0378d7d4d7c035496c0bcd15730d.

--- utils/automation_scripts/main.sh
+++ utils/automation_scripts/main.sh
@@ -53,7 +53,7 @@ comment()
 	string=`echo "$line" | awk -F : '{ print $2 }'`
 	num2=$(($num+3))
 
-	if [ ${string:0:1} != '#' -a ${string} != '\n']
+	if [ ${string:0:1} != '#' ]
 	then
 		sudo sed -i "$num,$num2 s/^/#/g" $fileName
 	fi
