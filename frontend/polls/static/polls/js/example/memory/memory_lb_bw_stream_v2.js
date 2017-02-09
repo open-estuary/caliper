91149ed24f4420144d30063df25ee3e22c02faed
Shyju PV <shyju.pv@huawei.com>
2017-02-09 22:48:10 +0530

    lmbench changes for frontend folder, for html report

---
+++ frontend/polls/static/polls/js/example/memory/memory_lb_bw_stream_v2.js
@@ -0,0 +1,21 @@
+
+$(function () {
+    var test = document.getElementById("memory_tst").value;  
+    
+    var bandwidth_dic = getJson(test, 'lb_bw_stream_v2')
+    var columns = getHoriColumn(bandwidth_dic);
+    var data = getHoriData(bandwidth_dic, columns);
+
+    // initialize grid
+    var options = {emptyRow: true, sortable: false};
+	var grid = $(document.getElementById("memory_lb_bw_stream_v2")).grid(data, columns, options);
+    draw_grid(grid);
+    
+    // api examples
+    var $row = grid.getRowByIndex(0);
+    console.group("data api examples");
+    console.log("grid.getGridData():", grid.getGridData());
+    console.groupEnd();
+
+    window.grid = grid;
+});
