eefedfe361d0a4e2ee8420d1f280a5e7cea90fd5
yangtt <yangtt0509@thundersoft.com>
2017-08-30 13:42:54 +0800

    update upload

--- upload/upload.py
+++ upload/upload.py
@@ -5,6 +5,7 @@ import urllib
 import urllib2
 import shutil
 import os,tarfile
+import json
 from caliper.client.shared import caliper_path
 import caliper.server.utils as server_utils
 
@@ -19,7 +20,7 @@ def upload_result(target):
     :return: None
     '''
     #workspace dir path for the test, for example: /home/fanxh/caliper_output/hansanyang-OptiPlex-3020_WS_17-05-03_11-29-29
-    dirpath = caliper_path.workspace;
+    dirpath = caliper_path.WORKSPACE;
 
     #dir path for score, for example: /home/fanxh/caliper_output/frontend/frontend/data_files/Normalised_Logs
     dir_score_path = caliper_path.HTML_DATA_DIR_OUTPUT
@@ -35,8 +36,10 @@ def upload_result(target):
 
 
 def upload_and_savedb(dirpath,json_path_source):
-    # tar file      
-    shutil.rmtree(os.path.join(dirpath,"binary"))
+    # tar file 
+    bin_file = os.path.exists(os.path.join(dirpath,"binary"))
+    if bin_file:   
+        shutil.rmtree(os.path.join(dirpath,"binary"))
     json_path=os.path.join(dirpath,os.path.basename(json_path_source))
     shutil.copyfile(json_path_source,json_path)
     output_file=dirpath+".tar.gz"
@@ -45,21 +48,23 @@ def upload_and_savedb(dirpath,json_path_source):
     # upload
     register_openers()
     datagen, headers = multipart_encode({'file':open(output_file, 'rb')})
-    request = urllib2.Request('http://localhost:8000/test_post', datagen, headers)
+    request = urllib2.Request('http://192.168.64.157:8000/test_post', datagen, headers)
     response = urllib2.urlopen(request)
     save_path = response.read()
 
     # save db
-    db_values={"save_path":save_path,"json_path":json_path}
+    with open(json_path,'r') as load_f:
+        json_data = json.load(load_f)
+    db_values={"save_path":save_path,"json_data":json_data}
     db_data = urllib.urlencode(db_values)
-    db_url = "http://localhost:8000/save_data"
+    db_url = "http://192.168.64.157:8000/save_data"
     db_request = urllib2.Request(db_url,db_data)
     db_response = urllib2.urlopen(db_request)
     print db_response.read()
 
 # example
 #dirpath = "C:\\Users\\yangtt\\Desktop\\fanxh-OptiPlex-3020_WS_17-08-07_11-03-46"
-dirpath = caliper_path.workspace;
+# dirpath = caliper_path.workspace;
 #json_path_source="C:\\Users\\yangtt\\Desktop\\Normalised_Logs\\ts-OptiPlex-3020_score_post.json"
-json_path_source = caliper_path.HTML_DATA_DIR_OUTPUT
-upload_and_savedb(dirpath,json_path_source)
+# json_path_source = caliper_path.HTML_DATA_DIR_OUTPUT
+# upload_and_savedb(dirpath,json_path_source)
