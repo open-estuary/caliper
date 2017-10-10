4d71f1342f37008ce2d6212ac05e0c7fb562fd9a
hansy <hansy0419@thundersoft.com>
2017-10-10 19:52:45 +0800

    can use arm normal user to install caliper

--- test_cases_cfg/common/fio/fio_build.sh
+++ test_cases_cfg/common/fio/fio_build.sh
@@ -2,7 +2,7 @@ build_fio()
 {
     CURDIR=$(cd `dirname $0`; pwd)
     pushd ${CURDIR}/benchmarks/fio/ansible > /dev/null
-    ansible-playbook -i ~/caliper_output/configuration/config/hosts site.yml
+    ansible-playbook -i ~/caliper_output/configuration/config/hosts site.yml -u root
     popd > /dev/null
 }
 
--- test_cases_cfg/common/stressng/stressng_build.sh
+++ test_cases_cfg/common/stressng/stressng_build.sh
@@ -2,7 +2,7 @@ build() {
 
     CURDIR=$(cd `dirname $0`; pwd)
     pushd ${CURDIR}/benchmarks/stressng/ansible > /dev/null
-    ansible-playbook -i ~/caliper_output/configuration/config/hosts site.yml
+    ansible-playbook -i ~/caliper_output/configuration/config/hosts site.yml -u root
     popd > /dev/null
 
 }
